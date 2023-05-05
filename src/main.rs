e7332a5683611f99bf8ec5bfb8615c96ea653d41
jrp <jrpacheco.dev@gmail.com>
2023-05-05 20:09:36 +0100

    feat: basic endpoints

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::{
+    response::status,
+    serde::json::{json, Value},
+};
+
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    json!([{"id": 1, "name": "John Doe"}, {"id": 2, "name": "Jane Doe"}])
+}
+
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> Value {
+    json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
+}
+
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32) -> status::NoContent {
+    status::NoContent
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!({"status": "error", "reason": "Resource was not found."})
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount(
+            "/",
+            routes![
+                get_rustaceans,
+                view_rustacean,
+                create_rustacean,
+                update_rustacean,
+                delete_rustacean,
+            ],
+        )
+        .register("/", catchers![not_found])
+        .launch()
+        .await;
+}
